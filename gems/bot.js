0265f96ccad5ae019d85701e3f9d949f38ead217
João Lopes e Silva <jpedro.e.silva@gmail.com>
2015-05-23 16:45:40 +0200

    Incorporating inheritance from the Character class in the Bot class

--- gems/bot.js
+++ gems/bot.js
@@ -13,6 +13,8 @@ var BotModule = (function () {
         this.viewing_range = 100;
         this.view_points = [];
     };
+    
+    Bot.inheritsFrom(Character);
     Bot.prototype.Place = function(handler, _x, _y)
     {
         // Capure handler
@@ -30,23 +32,7 @@ var BotModule = (function () {
         this.body.bot = this;
     };
     
-    Bot.prototype.Update = function()
-    {
-       if (this.brain !== undefined)
-           this.brain.Think(this);
-    };
-    Bot.prototype.PlugBrain = function(_brain)
-    {
-        this.brain = _brain;
-    };
-    Bot.prototype.AddVelocityX = function(speed)
-    {
-      this.body.data.velocity[0] += this.body.world.pxmi(speed);
-    };
-    Bot.prototype.AddVelocityY = function(speed)
-    {
-      this.body.data.velocity[1] += this.body.world.pxmi(speed);
-    };
+
     Bot.prototype.getHit = function(body)
     {
       body.bot.health -= 10;
@@ -56,69 +42,10 @@ var BotModule = (function () {
     }
     
     // Brain Class
-    var Brain = function(){
+    var BotBrain = function(){
     };
-    Brain.prototype.Think = function(subject)
-    {
-        if(!subject.external_move)
-            subject.body.setZeroVelocity(); 
-        
-        
-        var rnd_change_move = subject.handler_ref.rnd.integerInRange(0, 100);
-        if(rnd_change_move > 99)
-        {
-            var rnd_dir = subject.handler_ref.rnd.integerInRange(0, 100);
-            if(rnd_dir >= 0 && rnd_dir < 25)
-                subject.dir = 0;
-            else if(rnd_dir >= 25 && rnd_dir < 50)
-                subject.dir = 1;
-            else if(rnd_dir >= 50 && rnd_dir < 75)
-                subject.dir = 2;
-            else if(rnd_dir >= 75 && rnd_dir < 100)
-                subject.dir = 3;
-        }
-        
-        if(subject.dir === 0)
-            subject.AddVelocityX(-50);
-        else if(subject.dir === 1)
-            subject.AddVelocityX(50);
-        else if(subject.dir === 2)
-            subject.AddVelocityY(-50);
-        else if(subject.dir === 3)
-            subject.AddVelocityY(50);
-        
-        subject.external_move = false;
-    };
+    BotBrain.inheritsFrom(Brain);
     
-    // Brain Class
-    var PlayerBrain = function(_cursor_keys){
-        this.cursor_keys = _cursor_keys;
-    };
-    PlayerBrain.prototype.Think = function(subject)
-    {
-        subject.body.setZeroVelocity();
-        
-        var delta_mouse_rad = subject.body.rotation - subject.handler_ref.physics.arcade.angleToPointer(subject.sprite) - Math.PI/2;  
-        delta_mouse_rad = delta_mouse_rad - Math.PI/2;
-        var mod = Math.PI * 2;
-        delta_mouse_rad = delta_mouse_rad % mod; // saturate value to [-Math.PI*2,Math.PI*2]
-
-        if (delta_mouse_rad !== delta_mouse_rad % (mod/2) ) { 
-          delta_mouse_rad = (delta_mouse_rad < 0) ? delta_mouse_rad + mod : delta_mouse_rad - mod;
-        }
-        var speed = 650;
-        subject.body.rotateLeft(speed * delta_mouse_rad);
-        
-        if (this.cursor_keys.left.isDown)
-            subject.AddVelocityX(-200);
-        else if (this.cursor_keys.right.isDown)
-            subject.AddVelocityX(200);
-
-        if (this.cursor_keys.up.isDown)
-            subject.AddVelocityY(-200);
-        else if (this.cursor_keys.down.isDown)
-           subject.AddVelocityY(200);
-    };
     
     // Public interface
     return {
--- gems/proto_character.js
+++ gems/proto_character.js
@@ -1,5 +1,5 @@
 /*
- * Prototype Class from which any movable / intellingent entity should inherit
+ * Prototype Classes CHARACTER and BRAIN from which any movable / intelligent entity should inherit its body and its brain
  *      ex: monsters, npcs, human-players
  *
  * This file should have no public interface.
@@ -34,39 +34,11 @@ Character.prototype.AddVelocityY = function(speed)
 {
   this.body.data.velocity[1] += this.body.world.pxmi(speed);
 };
-Bot.prototype.updateVision = function(layer)
-{
-    this.view_points = [];
-    for(var a = 0; a < Math.PI * 2; a += Math.PI/90) {
-        // Create a ray from the light to a point on the circle
-        var ray = new Phaser.Line(this.body.x, this.body.y, this.body.x + Math.cos(a) * this.viewing_range, 
-            this.body.y + Math.sin(a) * this.viewing_range);
-        this.view_points.push(getRayCastPoints(layer, ray));
-    }
-}
-
-var getRayCastPoints = function (layer, ray) {
-    var tiles = layer.getTiles(ray.x, ray.y, ray.width, ray.height, false, false);
-    if (tiles.length !== 0)
-    {
-        var coords = ray.coordinatesOnLine(4);
-        for (var t = 0; t < coords.length; t++)
-        {
-            for (var i = 0; i < tiles.length; i++)
-            {
-                if (tiles[i].collides && tiles[i].containsPoint(coords[t][0], coords[t][1]))
-                {
-                    return {x:coords[t][0],y:coords[t][1]};
-                }
-            }
-        }
-    }
-    return {x:ray.end.x,y:ray.end.y};
-};
 
 // Brain Class
 var Brain = function(){
 };
+
 Brain.prototype.Think = function(subject)
 {
     if(!subject.external_move)
@@ -99,32 +71,3 @@ Brain.prototype.Think = function(subject)
     subject.external_move = false;
 };
 
-// Brain Class
-var PlayerBrain = function(_cursor_keys){
-    this.cursor_keys = _cursor_keys;
-};
-PlayerBrain.prototype.Think = function(subject)
-{
-    subject.body.setZeroVelocity();
-
-    var delta_mouse_rad = subject.body.rotation - subject.handler_ref.physics.arcade.angleToPointer(subject.sprite) - Math.PI/2;  
-    delta_mouse_rad = delta_mouse_rad - Math.PI/2;
-    var mod = Math.PI * 2;
-    delta_mouse_rad = delta_mouse_rad % mod; // saturate value to [-Math.PI*2,Math.PI*2]
-
-    if (delta_mouse_rad !== delta_mouse_rad % (mod/2) ) { 
-      delta_mouse_rad = (delta_mouse_rad < 0) ? delta_mouse_rad + mod : delta_mouse_rad - mod;
-    }
-    var speed = 650;
-    subject.body.rotateLeft(speed * delta_mouse_rad);
-
-    if (this.cursor_keys.left.isDown)
-        subject.AddVelocityX(-200);
-    else if (this.cursor_keys.right.isDown)
-        subject.AddVelocityX(200);
-
-    if (this.cursor_keys.up.isDown)
-        subject.AddVelocityY(-200);
-    else if (this.cursor_keys.down.isDown)
-       subject.AddVelocityY(200);
-};
